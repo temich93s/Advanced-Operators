dbb1ff0ed53027786155aeab36f68c017016426b
Temich93s <nil>
2021-10-16 07:19:28 +1000

    Пройдена тема: Побитовый оператор AND

--- Advanced Operators/main.swift
+++ Advanced Operators/main.swift
@@ -17,3 +17,11 @@ let initialBits: UInt8 = 0b00001111
 let invertedBits = ~initialBits  // равен 11110000
 print(initialBits, invertedBits)
 
+
+//MARK: Побитовый оператор AND
+print("\n//Побитовый оператор AND")
+
+let firstSixBits: UInt8 = 0b11111100
+let lastSixBits: UInt8  = 0b00111111
+let middleFourBits = firstSixBits & lastSixBits  // равен 00111100
+print(firstSixBits, lastSixBits, middleFourBits)
