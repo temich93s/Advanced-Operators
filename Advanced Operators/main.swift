4e9cf9fa08a005a2471fbf1b658900920f54f002
Temich93s <nil>
2021-10-16 08:03:55 +1000

    Пройдена тема: Операторные функции

--- Advanced Operators/main.swift
+++ Advanced Operators/main.swift
@@ -91,3 +91,25 @@ print(2 + 3 % 4 * 5)
 
 print(2 + ((3 % 4) * 5))
 // это равно 17
+
+
+//MARK: Операторные функции
+print("\n//Операторные функции")
+
+struct Vector2D {
+    var x = 0.0, y = 0.0
+}
+ 
+extension Vector2D {
+    static func + (left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y + right.y)
+    }
+}
+
+let vector = Vector2D(x: 3.0, y: 1.0)
+let anotherVector = Vector2D(x: 2.0, y: 4.0)
+let combinedVector = vector + anotherVector
+// combinedVector является экземпляром Vector2D, который имеет значения (5.0, 5.0)
+
+print(vector, anotherVector, combinedVector, separator: "\n")
+
